23adfca0ea7a2862d643f20934aeb01012060585
Svi-Kos <svitlanakos@gmail.com>
2020-09-24 13:10:40 +0300

    додає розмітку галереї та модальне вікно
    відкриття, закриття на кнопку, з клав-ри на Esc, кліком по оверлею

--- js/task.js
+++ js/task.js
@@ -1,3 +1,5 @@
+// додаю галерею
+
 import gallery from "./gallery-items.js";
 
 {
@@ -19,18 +21,18 @@ import gallery from "./gallery-items.js";
 const makeGalleryItemMarkup = ({ preview, original, description }) => {
   return `
     <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>
+      <a
+        class="gallery__link"
+        href=""
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
     `;
 };
 
@@ -39,3 +41,47 @@ const imageGalleryEl = document.querySelector(".js-gallery");
 const makeGalleryMarkup = gallery.map(makeGalleryItemMarkup).join("");
 
 imageGalleryEl.insertAdjacentHTML("afterbegin", makeGalleryMarkup);
+
+// тимчасовий оверлей контент
+
+const lightboxContentEl = document.querySelector(".lightbox__content img");
+
+lightboxContentEl.src =
+  "https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg";
+
+lightboxContentEl.alt = "Tulips";
+
+// закрити відкрити модальне вікно
+
+const refs = {
+  openLightbox: document.querySelector(".js-gallery"),
+  closeLightbox: document.querySelector('[data-action="close-lightbox"]'),
+  lightbox: document.querySelector(".js-lightbox"),
+  overlay: document.querySelector(".lightbox__overlay"),
+};
+
+refs.openLightbox.addEventListener("click", onOpenLightbox);
+refs.closeLightbox.addEventListener("click", onCloseLightbox);
+refs.overlay.addEventListener("click", onOverlayClick);
+window.addEventListener("keydown", onEscKey);
+
+function onOpenLightbox(event) {
+  window.addEventListener("keydown", onEscKey);
+  refs.lightbox.classList.add("is-open");
+  event.preventDefault();
+}
+
+function onCloseLightbox() {
+  window.removeEventListener("keydown", onEscKey);
+  refs.lightbox.classList.remove("is-open");
+}
+
+function onOverlayClick() {
+  onCloseLightbox();
+}
+
+function onEscKey(event) {
+  if (event.code === "Escape") {
+    onCloseLightbox();
+  }
+}
